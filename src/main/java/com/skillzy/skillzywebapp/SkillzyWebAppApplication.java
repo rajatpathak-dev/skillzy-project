f160f8c0745962859dcf7139c5a934afe6d70a02
Rajat Pathak <rajatpathak9888@gmail.com>
2025-02-21 13:02:50 +0530

    uploadng project structure to github

---
+++ src/main/java/com/skillzy/skillzywebapp/SkillzyWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.skillzy.skillzywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillzyWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillzyWebAppApplication.class, args);
+	}
+
+}
